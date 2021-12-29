df839a1b61df74aa9b42e3d09b312a4ca741ae4f
Nate Wilcox <nejucomo+dev@gmail.com>
2021-12-29 09:31:57 -0700

    Streamline the example code.

--- web_render_hex_board/src/lib.rs
+++ web_render_hex_board/src/lib.rs
@@ -6,16 +6,9 @@ pub fn main() -> Result<(), JsValue> {
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
     val.set_inner_html("Hello from Rust!");
-
     body.append_child(&val)?;
 
     Ok(())
-}
-
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
 }
