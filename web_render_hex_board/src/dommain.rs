ff31c0398213759871721dab518a9f68b28279de
Nate Wilcox <nejucomo+dev@gmail.com>
2021-12-29 13:39:06 -0700

    Make the canvas full-ish size and draw a border.

--- web_render_hex_board/src/dommain.rs
+++ web_render_hex_board/src/dommain.rs
@@ -10,7 +10,19 @@ pub fn main() -> Result<(), JsValue> {
     let body = dom_call!(document.body())?;
 
     let canvas: HtmlCanvasElement = jscast(document.create_element("canvas")?)?;
+    let w = body.client_width();
+    let h = body.client_height();
+    canvas.set_width(u32::try_from(w).unwrap());
+    canvas.set_height(u32::try_from(h).unwrap());
+
     let ctx: CanvasRenderingContext2d = jscast(dom_call!(canvas.get_context("2d")?)?)?;
+    ctx.rect(
+        0f64,
+        0f64,
+        f64::try_from(w).unwrap() * 0.98,
+        f64::try_from(h).unwrap() * 0.90,
+    );
+    ctx.stroke();
     ctx.rect(10f64, 10f64, 30f64, 20f64);
     ctx.stroke();
 
