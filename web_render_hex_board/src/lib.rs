6b97193f0402d51c949dd9d05778e051716956e1
Nate Wilcox <nejucomo+dev@gmail.com>
2021-12-29 09:53:13 -0700

    Introduce an error-handling macro.

--- web_render_hex_board/src/lib.rs
+++ web_render_hex_board/src/lib.rs
@@ -1,10 +1,22 @@
 use wasm_bindgen::prelude::*;
 
+macro_rules! dom_call {
+    ( $x:expr ) => {
+        match $x {
+            Some(v) => Ok(v),
+            None => Err(JsValue::from_str(&format!(
+                "DOM Error: {} returned None",
+                stringify!($x)
+            ))),
+        }
+    };
+}
+
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    let window = dom_call!(web_sys::window())?;
+    let document = dom_call!(window.document())?;
+    let body = dom_call!(document.body())?;
 
     let val = document.create_element("p")?;
     val.set_inner_html("Hello from Rust!");
